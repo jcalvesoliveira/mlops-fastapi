b023afd0f588a5563e1faa78ff48589eb1c3739e
jcalvesoliveira <jcalvesoliveira@hotmail.com>
2023-05-25 09:50:47 -0300

    video3.3

---
+++ mlops/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from app.models import House
+from app.services import HousePrices
+
+app = FastAPI()
+
+house_prices = HousePrices()
+house_prices.load_model()
+
+@app.get("/")
+def home():
+    return {"First": "API"}
+
+@app.post("/predict")
+def predict(house: House) -> float:
+    house_prices.predict(house)
